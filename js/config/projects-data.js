26838dba693408881a3ffaf600480c07f63c1a42
anjinhoooo <131448950+Anjins@users.noreply.github.com>
2025-11-11 20:00:57 +0000

    API key

--- js/config/projects-data.js
+++ js/config/projects-data.js
@@ -9,7 +9,7 @@ const projects = [
 
 const filterOptions = ["All", "Web Design", "Branding", "UI/UX Design", "Development", "Photography", "Motion Graphics"];
 
-const PEXELS_API_KEY = 'api_key';
+const PEXELS_API_KEY = 'J0g5urDSko7IAE9f7FQqFY07J7o57gfMCu8WmErEJC5mnmqvSFxkhc9c';
 
 // Mapeamento de temas para queries de pesquisa na API
 const themeQueries = {
